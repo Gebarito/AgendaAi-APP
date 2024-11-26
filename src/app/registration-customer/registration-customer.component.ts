bd75019d1ea34b8c4be35c6224a1af51447686e6
Gebarito <joaopedrogebara@gmail.com>
2024-11-26 02:12:35 -0300

    fix: Corrige erros visuais no login de clientes

--- src/app/registration-customer/registration-customer.component.ts
+++ src/app/registration-customer/registration-customer.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration-customer',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './registration-customer.component.html',
   styleUrl: './registration-customer.component.scss'
 })
-export class RegistrationCustomerComponent {}
+
+export class RegistrationCustomerComponent {
+  haveAccount: boolean = true;
+
+  toggleHaveAccount() {
+    this.haveAccount = !this.haveAccount;
+  }
+}
