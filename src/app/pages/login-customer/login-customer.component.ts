08de2fed3154035536365c773daff2b2fc186b14
Gebarito <joaopedrogebara@gmail.com>
2024-11-25 22:46:11 -0300

    feat: Cria arquivos da página login-customer

---
+++ src/app/pages/login-customer/login-customer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-customer.component.html',
+  styleUrl: './login-customer.component.scss'
+})
+export class LoginCustomerComponent {
+
+}
