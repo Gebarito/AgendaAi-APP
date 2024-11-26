edfa8b140fd9213efdf5a9a0cd52a595fadb18f3
Gebarito <joaopedrogebara@gmail.com>
2024-11-26 11:20:31 -0300

    feat: Implementa sistema de autenticação com observables

--- src/app/registration-customer/registration-customer.component.ts
+++ src/app/registration-customer/registration-customer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { CommonModule } from '@angular/common';
-
+import { AuthService } from '../authentication/AuthService';
 @Component({
   selector: 'app-registration-customer',
   standalone: true,
@@ -12,8 +12,40 @@ import { CommonModule } from '@angular/common';
 
 export class RegistrationCustomerComponent {
   haveAccount: boolean = true;
+  apiUrlBase: string = 'http://localhost:8080/customer/';
+
+  constructor(
+    private authService: AuthService
+  ){}
 
   toggleHaveAccount() {
     this.haveAccount = !this.haveAccount;
   }
+
+  validateFormLogin(form: any) {
+    return(
+      (form.value.email !== '' || form.value.email !== null || !form.value.email) &&
+      (form.value.password !== '' || form.value.password !== null || !form.value.cpf)
+    );
+  }
+
+
+  validateCredentials(form: any, response: any) {
+    return ((form.cpf.value === response.cpf.value) && (form.password.value === response.password.value));
+  }
+
+  login(form: any) {
+    if (!this.validateFormLogin(form)) {
+      console.error('Formulario inválido');
+      return;
+    }
+
+    this.authService.login(); 
+  }
+
+  logout():void {
+    this.authService.logout();
+  }
+
+
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RegistrationCustomerComponent } from '../../registration-customer/registration-customer.component';
+import { AuthService } from '../../authentication/AuthService';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private authService: AuthService
+  ){}
+
+  ngOnInit() {
+    this.authService.isLoggedIn$.subscribe(status => {
+      this.isLoggedIn = status;
+    })
+  }
+
+}
