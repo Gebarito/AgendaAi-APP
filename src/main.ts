65c018d8f57c31bdf8aa1126ebf5cdaa2646bedf
Gebarito <joaopedrogebara@gmail.com>
2024-11-26 21:18:46 -0300

    feat: Cria rota para profile

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app/home/app.component';
 import { provideRouter } from '@angular/router';
 import { RegistrationCustomerComponent } from './app/registration-customer/registration-customer.component';
 import { RegistrationBusinessComponent } from './app/registration-business/registration-business.component';
+import { ProfileComponent } from './app/profile/profile.component';
 
 const routes = [
   { path: '', component: AppComponent },
   { path: 'customer', component: RegistrationCustomerComponent },
   { path: 'business', component: RegistrationBusinessComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 bootstrapApplication(AppComponent, {
