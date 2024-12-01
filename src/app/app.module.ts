549fa4f4f4e0c5ad0924001fba62ea97b34d6206
Gebarito <joaopedrogebara@gmail.com>
2024-12-01 13:04:30 -0300

    feat: Cria componentes standalones dos 4 cruds de caso de uso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { JobsComponent } from './components/jobs/jobs.component';
+import { BusinessComponent } from './components/business/business.component';
+import { OrderComponent } from './components/order/order.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JobsComponent
+    JobsComponent,
+    BusinessComponent,
+    OrderComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
